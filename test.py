326262370ad59ff4734756d5cb9810b228ab6926
Hoang604 <dinhviethoang604@gmail.com>
2025-08-07 16:52:33 +0700

    rm test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from data_processing.fetch_raw_data import DataFetcher
-
-fetcher = DataFetcher('HPG')
-
-data = fetcher.fetch_company_balance_sheet()
-import pandas as pd
-pd.set_option('display.max_columns', None)  # Show all columns in DataFrame
-print(data)
